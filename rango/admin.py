4999e98e38c147bcd87e338931ebbe0bbcc522d1
GUANGHUI <2490629m@student.gla.ac.uk>
2020-02-06 14:47:08 +0000

    modelviewtemplate

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
+
+
+
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category','url')
 
-admin.site.register(Category)
+#this class for customising the admin interface
+# update the registration to include this customised interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
