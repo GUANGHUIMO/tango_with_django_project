2fb1ff62b30cb76cc50fd00d55b57327ee43898e
GUANGHUI <2490629m@student.gla.ac.uk>
2020-02-06 01:10:21 +0000

    database

--- rango/views.py
+++ rango/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    #return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+    # Refer to the TwD book for more information on how this updated view works.
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
+
 def about(request):
     # Spoiler: you don't need to pass a context dictionary here.
-    return render(request, 'rango/about.html')#不需要context，因为是用来传string的。
+    return render(request, 'rango/about.html')
 
 
 
