1558951153f3a4be7b85839fd5816829be285dae
GUANGHUI <2490629m@student.gla.ac.uk>
2020-02-07 14:12:40 +0000

    workwithtemplate

--- rango/views.py
+++ rango/views.py
@@ -4,9 +4,8 @@ from rango.models import Category
 from rango.models import Page
 from rango.forms import CategoryForm
 from django.shortcuts import redirect
-from rango.forms import PageForm
-from django.shortcuts import redirect
 from django.urls import reverse
+from rango.forms import PageForm
 
 
 def index(request):
@@ -42,6 +41,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+
 def add_category(request):
     form = CategoryForm()
 
@@ -50,7 +50,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
 
@@ -65,7 +65,7 @@ def add_page(request, category_name_slug):
 
     # You cannot add a page to a Category that does not exist... DM
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
